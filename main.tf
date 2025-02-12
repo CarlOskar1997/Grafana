080669311c55acaaf5687b6cbfd2f94970976849
CarlOskar1997 <carloskar1905@gmail.com>
2025-02-12 15:51:47 +0100

    Lagt til yaml

--- main.tf
+++ main.tf
@@ -6,10 +6,14 @@ terraform {
       }
    }
 }
-
+resource "github_actions_secret" "grafana" {
+  repository       = "grafana"
+  secret_name      = "GRAFANA_KEY"
+  encrypted_value  = var.GRAFANA_KEY
+}
 provider "grafana" {
    alias = "cloud"
 
    url   = "https://carloskar1905.grafana.net/"
-#   auth  = 
+   auth  = github_actions_secret.encrypted_value
 }
