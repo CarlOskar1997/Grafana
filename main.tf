f8988ac62801b4f4fb8d3b95cf8748e3a0354d4a
CarlOskar1997 <carloskar1905@gmail.com>
2025-02-12 15:20:57 +0100

    funker dette endelig?

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+   required_providers {
+      grafana = {
+         source  = "grafana/grafana"
+         version = ">= 2.9.0"
+      }
+   }
+}
+
+provider "grafana" {
+   alias = "cloud"
+
+   url   = "https://carloskar1905.grafana.net/"
+}
