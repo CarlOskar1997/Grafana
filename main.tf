79aca400287027579fc97bf59b7fc3432f42184b
CarlOskar1997 <carloskar1905@gmail.com>
2025-02-13 10:43:53 +0100

    muligens fiksa

--- main.tf
+++ main.tf
@@ -7,14 +7,10 @@ terraform {
    }
 }
 
-resource "github_actions_secret" "GRAFANA_KEY" {
-  repository       = "Grafana"
-  secret_name      = "GRAFANA_KEY"
-}
 
 provider "grafana" {
    alias = "cloud"
 
    url   = "https://carloskar1905.grafana.net/"
-   auth  = github_actions_secret.secret_name
+   auth  = grafana_key
 }
